867021a581faceac3af44c486f7d8dcecb4d7af3
CodeSnakes <yichangkong@outlook.com>
2023-03-19 01:47:08 +0800

    commit type conversion

---
+++ typeConversion.py
@@ -0,0 +1,17 @@
+# type conversion
+birth_year = input("Enter your birth year: ")
+age = 2022 - int(birth_year)
+print(age)
+int()
+float()
+bool()
+str()
+first = input("First: ")
+# 10
+second = input("Second: ")
+# 20
+sum = first + second
+print(sum)
+# 1024
+
+
