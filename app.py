0c3c1eb4186c7a5bbf57c6308d56892299509ec9
CodeSnakes <yichangkong@outlook.com>
2023-03-19 01:44:37 +0800

    commit type conversion

---
+++ app.py
@@ -0,0 +1,13 @@
+print("hello world")
+# variables
+age = 20
+price = 19.53
+first_name = "Long"
+is_online = False
+print(age)
+# receiving input
+name = input("What are your name? ")
+print("Hello " + name)
+
+
+
